5c3467fddef50c7d3d3facb3fe9b53fdeee1c358
工作 <licheng6256@tutamail.com>
2026-03-19 16:37:54 +0800

    任务系统调整

--- config/log.php
+++ config/log.php
@@ -637,4 +637,20 @@ return [
             ]
         ],
     ],
+    'plugin.webman.redis-queue.default' => [
+        'handlers' => [
+            [
+                'class' => Monolog\Handler\RotatingFileHandler::class,
+                'constructor' => [
+                    runtime_path() . '/logs/redis_queue.log',
+                    7, //$maxFiles
+                    Monolog\Logger::DEBUG,
+                ],
+                'formatter' => [
+                    'class' => Monolog\Formatter\LineFormatter::class,
+                    'constructor' => [null, 'Y-m-d H:i:s', true],
+                ],
+            ]
+        ],
+    ],
 ];
