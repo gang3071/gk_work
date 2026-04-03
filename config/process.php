6280d6e58c2f1d94e92e8b2e1de87e8ee4ad800b
工作 <licheng6256@tutamail.com>
2026-04-03 22:44:17 +0800

    iGaming单一钱包业务调整

--- config/process.php
+++ config/process.php
@@ -24,40 +24,11 @@ use process\ReconciliationTask;
 use process\ReverseWater;
 
 return [
-    // File update detection and automatic reload
-//    'monitor' => [
-//        'handler' => process\Monitor::class,
-//        'reloadable' => false,
-//        'constructor' => [
-//            // Monitor these directories
-//            'monitorDir' => array_merge([
-//                app_path(),
-//                config_path(),
-//                base_path() . '/addons',
-//                base_path() . '/process',
-//                base_path() . '/support',
-//                base_path() . '/plugin',
-//                base_path() . '/resource',
-//                base_path() . '/.env',
-//            ], glob(base_path() . '/plugin/*/app'), glob(base_path() . '/plugin/*/config'), glob(base_path() . '/plugin/*/api')),
-//            // Files with these suffixes will be monitored
-//            'monitorExtensions' => [
-//                'php', 'html', 'htm', 'env'
-//            ],
-//            'options' => [
-//                'enable_file_monitor' => !Worker::$daemonize && DIRECTORY_SEPARATOR === '/',
-//                'enable_memory_monitor' => DIRECTORY_SEPARATOR === '/',
-//            ]
-//        ]
-//    ],
     'ProfitSettlement' => [
         'handler' => ProfitSettlement::class
     ],
     'LotteryPoolSocket' => [
         'handler' => LotteryPoolSocket::class
-    ],
-    'OnlinePlayerSocket' => [
-        'handler' => OnlinePlayerSocket::class
     ],
     'LotteryRemind' => [
         'handler' => LotteryRemind::class
