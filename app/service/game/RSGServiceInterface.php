42f11479bfe5120718780ed6fa12f1b99f8fb4e3
工作 <licheng6256@tutamail.com>
2026-04-05 14:06:16 +0800

    电子游戏平台调整

--- app/service/game/RSGServiceInterface.php
+++ app/service/game/RSGServiceInterface.php
@@ -451,7 +451,7 @@ class RSGServiceInterface extends GameServiceFactory implements GameServiceInter
      * @return mixed
      * @deprecated 已迁移到 Redis Lua 原子脚本，此方法不再使用
      */
-    public function balance()
+    public function balance(): mixed
     {
         // RSG 使用单一钱包，余额统一管理，不需要此方法
         return 0;
@@ -463,7 +463,7 @@ class RSGServiceInterface extends GameServiceFactory implements GameServiceInter
      * @return mixed
      * @deprecated 已迁移到 RedisLuaScripts::atomicBet，此方法不再使用
      */
-    public function bet($data)
+    public function bet($data): mixed
     {
         // 已迁移到 RsgGameController 中使用 RedisLuaScripts::atomicBet
         throw new \RuntimeException('bet() 方法已废弃，请使用 RedisLuaScripts::atomicBet');
@@ -475,7 +475,7 @@ class RSGServiceInterface extends GameServiceFactory implements GameServiceInter
      * @return mixed
      * @deprecated 已迁移到 RedisLuaScripts::atomicCancel，此方法不再使用
      */
-    public function cancelBet($data)
+    public function cancelBet($data): mixed
     {
         // 已迁移到 RsgGameController 中使用 RedisLuaScripts::atomicCancel
         throw new \RuntimeException('cancelBet() 方法已废弃，请使用 RedisLuaScripts::atomicCancel');
@@ -487,7 +487,7 @@ class RSGServiceInterface extends GameServiceFactory implements GameServiceInter
      * @return mixed
      * @deprecated 已迁移到 RedisLuaScripts::atomicSettle，此方法不再使用
      */
-    public function betResulet($data)
+    public function betResulet($data): mixed
     {
         // 已迁移到 RsgGameController 中使用 RedisLuaScripts::atomicSettle
         throw new \RuntimeException('betResulet() 方法已废弃，请使用 RedisLuaScripts::atomicSettle');
@@ -499,7 +499,7 @@ class RSGServiceInterface extends GameServiceFactory implements GameServiceInter
      * @return mixed
      * @deprecated 已迁移到 RedisLuaScripts::atomicSettle，此方法不再使用
      */
-    public function reBetResulet($data)
+    public function reBetResulet($data): mixed
     {
         // 已迁移到 RsgGameController 中使用 RedisLuaScripts::atomicSettle
         throw new \RuntimeException('reBetResulet() 方法已废弃，请使用 RedisLuaScripts::atomicSettle');
@@ -511,7 +511,7 @@ class RSGServiceInterface extends GameServiceFactory implements GameServiceInter
      * @param $data
      * @return mixed
      */
-    public function gift($data)
+    public function gift($data): mixed
     {
         // RSG 平台不支持送礼功能
         throw new \RuntimeException('RSG 平台不支持 gift() 功能');
