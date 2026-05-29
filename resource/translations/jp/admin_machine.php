032ea0e0ef079f94ff7620c8a2a2bab23e5bca15
工作 <licheng6256@tutamail.com>
2026-05-29 15:52:17 +0800

    机器操作逻辑

--- resource/translations/jp/admin_machine.php
+++ resource/translations/jp/admin_machine.php
@@ -11,8 +11,11 @@ return [
     'machine_not_found' => 'マシンが見つかりません',
     'field_required' => 'フィールド名は必須です',
     'machine_ids_required' => 'マシンIDリストは必須です',
+    'commands_required' => 'コマンドリストは必須です',
+    'data_must_be_numeric' => 'データパラメータは数値である必要があります',
 
     // Success messages
     'cmd_sent_success' => 'コマンド送信成功',
     'state_updated_success' => 'ステータス更新成功',
+    'batch_cmd_completed' => 'バッチコマンド実行完了',
 ];
